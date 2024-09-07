e54646696013693e972b7f4e0a40381a0f178785
Naresh Vishram <58872658+nareshvishram@users.noreply.github.com>
2024-09-08 00:24:42 +0530

    Time: 65 ms (6.18%), Space: 44.6 MB (5.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,28 +1,28 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int min=Integer.MAX_VALUE,idx=0;
-        for(int i=0;i<strs.length;i++){
-            if(strs[i].length()<min){
-                min=strs[i].length();
-                idx=i;
-            }
-        }
-        System.out.println(idx+" "+min);
-        for(int i=strs[idx].length();i>=0;i--){
-            String curr=strs[idx].substring(0,i);
+        if(strs.length==0)
+            return "";
+        String curr=strs[0];
+        for(int i=curr.length();i>=0;i--){
+            String tmp=curr.substring(0,i);
+            System.out.println(tmp);
             boolean flag=false;
             for(int j=0;j<strs.length;j++){
-                if(!isMatch(curr,strs[j])){
+
+                if(!isMatch(tmp,strs[j])){
                     flag=true;
                     break;
                 }
             }
             if(!flag)
-                return curr;
+                return tmp;
         }
         return "";
     }
     private boolean isMatch(String s1,String s2){
+        System.out.println(s1+" "+s2);
+        if(s1.length()>s2.length())
+                return false;
         for(int i=0;i<s1.length() && i<s2.length();i++){
             if(s1.charAt(i)!=s2.charAt(i))
                 return false;
